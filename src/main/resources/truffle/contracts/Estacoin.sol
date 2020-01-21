834a14415eaca12d38c74565d9ad7b35aa123521
shukriev <dark00v3@gmail.com>
2020-01-21 09:36:46 +0200

    Updated Estacoin contract

--- src/main/resources/truffle/contracts/Estacoin.sol
+++ src/main/resources/truffle/contracts/Estacoin.sol
@@ -84,99 +84,98 @@ contract Estacoin is ERC20Interface, Owned, SafeMath {
     uint public _totalSupply;
     mapping(address => uint) balances;
     mapping(address => mapping(address => uint)) allowed;
-
-
+    
     constructor() public {
         symbol = "EC";
         name = "Estacoin";
         decimals = 18;
         _totalSupply = 100000000000000000000000000;
-        balances[0x63c14CaceD9e351ED8463E21D9bdee673b26fbbF] = _totalSupply;
-        emit Transfer(address(0), 0x63c14CaceD9e351ED8463E21D9bdee673b26fbbF, _totalSupply);
+        owner = address(0xd34F19725B97F7669090684b690812237f895e26);
+        
+        balances[owner] = _totalSupply;
+        emit Transfer(address(0), owner, _totalSupply);
     }
-
+    
     // Total supply
     function totalSupply() public view returns (uint) {
-        return _totalSupply  - balances[address(0)];
+        return balances[owner];
     }
-
+    
     // Get the token balance for account tokenOwner
     function balanceOf(address tokenOwner) public view returns (uint balance) {
      return balances[tokenOwner];
     }
-
+    
     // Transfer the balance from token owner's account to to account
     // - Owner's account must have sufficient balance to transfer
     // - 0 value transfers are allowed
-
+    
     function transfer(address to, uint tokens) public returns (bool success) {
         balances[msg.sender] = safeSub(balances[msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
         emit Transfer(msg.sender, to, tokens);
         return true;
     }
-
-    // TODO Wallet TO Wallet
-    // function transferWalletToWallet(address from, address to, uint tokens) public returns (bool success) {
-    //     balances[from] = safeSub(balances[from], tokens);
-    //     balances[to] = safeAdd(balances[to], tokens);
-    //     emit Transfer(from, to, tokens);
-    //     return true;
-    // }
+    
+    function transferWalletToWallet(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = safeSub(balances[from], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(from, to, tokens);
+        return true;
+    }
 
     // Token owner can approve for spender to transferFrom(...) tokens
     // from the token owner's account
     //
     // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
     // recommends that there are no checks for the approval double-spend attack
-    // as this should be implemented in user interfaces
+    // as this should be implemented in user interfaces 
     function approve(address spender, uint tokens) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
         return true;
     }
-
+    
     // Transfer tokens from the from account to the to account
-    //
+    // 
     // The calling account must already have sufficient tokens approve(...)-d
     // for spending from the from account and
     // - From account must have sufficient balance to transfer
     // - Spender must have sufficient allowance to transfer
     // - 0 value transfers are allowed
-
+    
     function transferFrom(address from, address to, uint tokens) public returns (bool success) {
         balances[from] = safeSub(balances[from], tokens);
-        // TODO Ignore allowance
-        // allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
         emit Transfer(from, to, tokens);
         return true;
     }
-
+    
     // Returns the amount of tokens approved by the owner that can be
     // transferred to the spender's account
     function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
         return allowed[tokenOwner][spender];
     }
-
+    
     // Token owner can approve for spender to transferFrom(...) tokens
     // from the token owner's account. The spender contract function
     // receiveApproval(...) is then executed
-
+    
     function approveAndCall(address spender, uint tokens) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
-
+        
         //TODO Call
-        //ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data)
+        //ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data)        
         return true;
     }
-
+    
     // Don't accept ETH
     function () external payable {
         revert();
     }
-
+    
     // Owner can transfer out any accidentally sent ERC20 tokens
     function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
         return ERC20Interface(tokenAddress).transfer(owner, tokens);
