0353479765e6510998e6e1ebe77e152f7e9c13e4
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-02-12 15:54:57 +0000

    Added logging to debug the bad versions release microservices

--- src/main/java/com/estafet/blockchain/demo/blockchain/gateway/ms/model/API.java
+++ src/main/java/com/estafet/blockchain/demo/blockchain/gateway/ms/model/API.java
@@ -1,16 +1,21 @@
 package com.estafet.blockchain.demo.blockchain.gateway.ms.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.StringTokenizer;
 
 public class API {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(API.class);
+	
 	private String version;
 
 	public API() {
 	}
 
 	public API(String version) {
-		this.version = version;
+		this.version = version;		
 	}
 
 	public String getVersion() {
@@ -18,11 +23,11 @@ public class API {
 		String p1 = tokenizer.nextToken();
 		String p2 = tokenizer.nextToken();
 		String p3 = tokenizer.nextToken();
+		LOGGER.info("version - " + version);
+		LOGGER.info("p1 - " + p1);
+		LOGGER.info("p2 - " + p2);
+		LOGGER.info("p3 - " + p3);
 		return p1 + "." + p2 + "." + Integer.toString(Integer.parseInt(p3) - 1);
-	}
-
-	public void setVersion(String version) {
-		this.version = version;
 	}
 
 }
